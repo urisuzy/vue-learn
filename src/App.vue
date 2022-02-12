6ea4d9db41c21b5cb669e7cc8fae779782de71e0
UriSuzy <webmaulana1@gmail.com>
2022-02-13 06:58:27 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="display: flex; justify-content: center">
+    <table border="1">
+      <thead>
+        <th>#</th>
+        <th>Name</th>
+        <th>Kelas</th>
+        <th>Pic</th>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in orangs" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.nama }}</td>
+          <td>{{ item.kelas }}</td>
+          <td><img :src="item.foto" alt="" width="150px" /></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    orangs: []
+  }),
+  methods: {
+    getUser(id) {
+      axios
+        .post(
+          'https://siama.sekolahan.id/api/v2/16/profilsiswa?idsiswa=' + id,
+          {},
+          {
+            headers: {
+              Authorization:
+                'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzaWFtYS5zZWtvbGFoYW4uaWQiLCJhdWQiOiIxMC4xMzAuMTMyLjI1MCIsImlhdCI6MTY0NDU4Mzc2MiwibmJmIjoxNjQ0NTgzNzcyLCJkYXRhIjp7ImlkIjpudWxsfX0.xIMMYSkrgPZPln2GJ2xpevg6XkQJ2xw_9auOLmwo1So'
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          if (res.data.responseData.results.nis != null) this.orangs.push(res.data.responseData.results)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    range(start, end) {
+      let ids = []
+      for (let i = start; i <= end; i++) {
+        ids.push(i)
+      }
+      return ids
+    }
+  },
+  mounted() {
+    console.log('anjay')
+    console.log(axios)
+    const ids = this.range(4500, 5500)
+    // this.getUser('6486')
+    for (let i in ids) {
+      this.getUser(ids[i])
+    }
   }
 }
 </script>
